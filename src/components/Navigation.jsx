59a8760dfec34d27439d0b8ac1d7c1a66e023d18
hadep275 <hadep275@gmail.com>
2023-12-08 19:48:04 -0700

    dropdown

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -7,37 +7,55 @@ import '../css/Navigation.css';
 
 const Navigation = () => {
   const [isNavOpen, setNavOpen] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
   const toggleNav = () => {
     console.log('Toggle nav called');
     setNavOpen(!isNavOpen);
   };
+
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
   
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 568);
+    };
+
+    // Set initial screen size
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <nav className={`nav ${isNavOpen ? 'open' : ''}`}>
       <div className="menu-toggle" onClick={toggleNav}>
-        {isNavOpen ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
+      <FontAwesomeIcon icon={isNavOpen ? faTimes : faBars} />
       </div>
-      {isNavOpen && (
-      <ul>
+      <ul className={isNavOpen ? 'show' : ''}>
       <li>
-          <Link to="about" smooth={true} duration={500} onClick={toggleNav}>
+          <Link to="about" smooth={true} duration={500} onClick={isSmallScreen ? closeNav : undefined}>
             About
           </Link>
         </li>
         <li>
-          <Link to="projects" smooth={true} duration={500} onClick={toggleNav}>
+          <Link to="projects" smooth={true} duration={500} onClick={isSmallScreen ? closeNav : undefined}>
             Projects
           </Link>
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500} onClick={toggleNav}>
+          <Link to="contact" smooth={true} duration={500} onClick={isSmallScreen ? closeNav : undefined}>
             Contact
           </Link>
         </li>
       </ul>
-      )}
       <ThemeToggle/>
 
     </nav>
