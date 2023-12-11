2efde0cd1b8c17e4adb6556a6c44e9f68fa25cc3
hadep275 <hadep275@gmail.com>
2023-12-10 17:08:36 -0700

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" className="section">
+      <div className="about-container">
+
       <h2>About Me</h2>
       <p>Your bio or description goes here.</p>
+      </div>
     </section>
   );
 };
