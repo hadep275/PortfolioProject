4bb3139f6c0abf72db2483e3315ac573e9ecd3b5
hadep275 <hadep275@gmail.com>
2023-12-15 00:12:50 -0700

    fit content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Element } from 'react-scroll';
 import { useTheme } from './ThemeProvider';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import project1Image from '../assets/img/project1.png';
 import project2Image from '../assets/img/project2.png';
 import project3Image from '../assets/img/project3.png';
@@ -14,21 +15,29 @@ const projects = [
     title: 'Project 1',
     description: 'Description of Project 1.',
     image: project1Image,
+    githubLink: 'https://github.com/your-username/project1',
+    demoLink: 'https://demo.project1.com',
   },
   {
     title: 'Project 2',
     description: 'Description of Project 2.',
     image: project2Image,
+    githubLink: 'https://github.com/your-username/project1',
+    demoLink: 'https://demo.project1.com',
   },
   {
     title: 'Project 3',
     description: 'Description of Project 3.',
     image: project3Image,
+    githubLink: 'https://github.com/your-username/project1',
+    demoLink: 'https://demo.project1.com',
   },
   {
     title: 'Project 4',
     description: 'Description of Project 4.',
     image: project4Image,
+    githubLink: 'https://github.com/your-username/project1',
+    demoLink: 'https://demo.project1.com',
   },
 ];
 
@@ -55,6 +64,14 @@ const Project = () => {
             <h3>{project.title}</h3>
             <img src={project.image} alt={`Project ${index + 1}`} />
             <p>{project.description}</p>
+            <div className="project-links">
+                <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                  <FaGithub /> 
+                </a>
+                <a href={project.demoLink} target="_blank" rel="noopener noreferrer">
+                  <FaExternalLinkAlt /> 
+                </a>
+              </div>
           </div> ))}
         </div>
       </section>
